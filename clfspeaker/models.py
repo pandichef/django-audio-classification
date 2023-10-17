c10f8ad1c28d77cfe1ca3d0ddac2f0f7ec7f543d
pandichef <rashtyda@gmail.com>
2023-10-16 21:44:20 -0500

    fix PA error

--- clfspeaker/models.py
+++ clfspeaker/models.py
@@ -8,18 +8,12 @@ from transformers import (
 )
 
 # tokenizer = AutoTokenizer.from_pretrained("facebook/wav2vec2-base")
-feature_extractor = AutoFeatureExtractor.from_pretrained("facebook/wav2vec2-base")
 model_path = os.path.join(
     os.path.dirname(os.path.abspath(__file__)), "facebook_wav2vec2-base_tuned",
 )
-
-model = AutoModelForAudioClassification.from_pretrained(model_path)
-clf = pipeline(
-    "audio-classification",
-    model=model,
-    # tokenizer=tokenizer,
-    feature_extractor=feature_extractor,
-)
+feature_extractor = None
+model = None
+clf = None
 
 
 def get_upload_to(instance, filename):
@@ -40,6 +34,23 @@ class Audio(models.Model):
         super(Audio, self).delete(*args, **kwargs)
 
     def identify_speaker(self):
+        # on PA, clf has to be defined here, otherwise the request will timeout
+        global feature_extractor
+        global model
+        global clf
+        if feature_extractor is None:
+            feature_extractor = AutoFeatureExtractor.from_pretrained(
+                "facebook/wav2vec2-base"
+            )
+        if model is None:
+            model = AutoModelForAudioClassification.from_pretrained(model_path)
+        if clf is None:
+            clf = pipeline(
+                "audio-classification",
+                model=model,
+                # tokenizer=tokenizer,
+                feature_extractor=feature_extractor,
+            )
         result = clf(self.audio_file.path)
         # print(result[0]["label"].split('_')[1])
         speaker_id = result[0]["label"].split("_")[1]
