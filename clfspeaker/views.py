8f499c68bf1bde9332a26352c833c958885298d4
pandichef <rashtyda@gmail.com>
2023-10-16 21:52:42 -0500

    add runtime

--- clfspeaker/views.py
+++ clfspeaker/views.py
@@ -3,10 +3,12 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from .forms import AudioForm
 from .models import Audio
+from time import time
 
 
 @csrf_exempt
 def upload_audio(request):
+    t0 = time()
     if request.method == "POST":
         form = AudioForm(request.POST, request.FILES)
         if form.is_valid():
@@ -18,7 +20,9 @@ def upload_audio(request):
             speaker = obj.identify_speaker()
             # process LLM model
             obj.delete()
-            return JsonResponse({"status": speaker})
+            return JsonResponse(
+                {"status": speaker + " (" + str(round(time() - t0, 1)) + " ms)"}
+            )
         else:
             return JsonResponse({"status": "error", "errors": form.errors}, status=400)
 
